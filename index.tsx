75cb61b539e92c9c6bf0698443165ffe4d6d2494
Pedro Figueiredo <pedro_augusto50@hotmail.com>
2026-01-30 16:51:34 -0300

    deploy fixes

--- index.tsx
+++ index.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const container = document.getElementById('root');
-if (container) {
-  const root = createRoot(container);
-  root.render(<App />);
+// Only run in browser environment
+if (typeof document !== 'undefined') {
+  const container = document.getElementById('root');
+  if (container) {
+    const root = createRoot(container);
+    root.render(<App />);
+  }
 }
